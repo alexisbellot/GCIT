f436a8f241402caf2bf845dda6736e0af6ac8d34
alexisbellot <45399314+alexisbellot@users.noreply.github.com>
2019-11-12 17:43:17 +0000

    Corrected typos

--- utils.py
+++ utils.py
@@ -4,8 +4,11 @@ from scipy.stats import wilcoxon
 import numpy as np
 import random
 from scipy import stats
-
-
+import time
+from collections import defaultdict
+import numpy as np
+import warnings
+from scipy.stats import rankdata
 
 
 
@@ -19,27 +22,22 @@ def negexp(x):
     return np.exp(-np.abs(x))
 
 
-def generate_samples_random(size=1000, sType='CI', dx=1, dy=1, dz=20, nstd=0.5, freq=1.0, fixed_function='linear',
+def generate_samples_random(size=1000, sType='CI', dx=1, dy=1, dz=20, nstd=1, fixed_function='linear',
                             debug=False, normalize = True, seed = None, dist_z = 'gaussian'):
     '''Generate CI,I or NI post-nonlinear samples
-    1. Z is independent Gaussian
+    1. Z is independent Gaussian or Laplace
     2. X = f1(<a,Z> + b + noise) and Y = f2(<c,Z> + d + noise) in case of CI
     Arguments:
         size : number of samples
-        sType: CI,I, or NI
+        sType: CI, I, or NI
         dx: Dimension of X
         dy: Dimension of Y
         dz: Dimension of Z
         nstd: noise standard deviation
-        freq: Freq of cosine function
-        f1,f2 an be within {x,x^2,x^3,tanh x, e^{-|x|}, cos x}
+        f1, f2 to be within {x,x^2,x^3,tanh x, e^{-|x|}, cos x}
 
     Output:
-        allsamples --> complete data-set
-    Note that:
-    [X = first dx coordinates of allsamples each row is an i.i.d samples]
-    [Y = [dx:dx + dy] coordinates of allsamples]
-    [Z = [dx+dy:dx+dy+dz] coordinates of all samples]
+        Samples X, Y, Z
     '''
     if seed == None:
         np.random.seed()
@@ -106,17 +104,17 @@ def generate_samples_random(size=1000, sType='CI', dx=1, dy=1, dz=20, nstd=0.5, 
 
     temp = Z * Ax
     m = np.mean(np.abs(temp))
-    nstd = nstd * m
+    nstd = nstd * m 
 
     if sType == 'CI':
-        X = f1(freq * (Z * Ax + nstd * np.random.multivariate_normal(np.zeros(dx), np.eye(dx), num)))
-        Y = f2(freq * (Z * Ay + nstd * np.random.multivariate_normal(np.zeros(dy), np.eye(dy), num)))
+        X = f1(Z * Ax + nstd * np.random.multivariate_normal(np.zeros(dx), np.eye(dx), num))
+        Y = f2(Z * Ay + nstd * np.random.multivariate_normal(np.zeros(dy), np.eye(dy), num))
     elif sType == 'I':
-        X = f1(freq * (nstd * np.random.multivariate_normal(np.zeros(dx), np.eye(dx), num)))
-        Y = f2(freq * (nstd * np.random.multivariate_normal(np.zeros(dy), np.eye(dy), num)))
+        X = f1(nstd * np.random.multivariate_normal(np.zeros(dx), np.eye(dx), num))
+        Y = f2(nstd * np.random.multivariate_normal(np.zeros(dy), np.eye(dy), num))
     else:
-        X = f1(freq * (np.random.multivariate_normal(np.zeros(dx), np.eye(dx), num)))
-        Y = f2(freq * (2 * X * Axy + Z * Ay + nstd * np.random.multivariate_normal(np.zeros(dy), np.eye(dy), num)))
+        X = np.random.multivariate_normal(np.zeros(dx), np.eye(dx), num)
+        Y = f2(2 * X * Axy + Z * Ay)
 
     if normalize == True:
         Z = (Z - Z.min()) / (Z.max() - Z.min())
@@ -236,4 +234,88 @@ Y = np.random.normal(0,2,500)
 kolmogorov(X,Y)
 '''
 
+def rdc(x, y, f=np.sin, k=20, s=1/6., n=1):
+    """
+    Computes the Randomized Dependence Coefficient
+    x,y: numpy arrays 1-D or 2-D
+         If 1-D, size (samples,)
+         If 2-D, size (samples, variables)
+    f:   function to use for random projection
+    k:   number of random projections to use
+    s:   scale parameter
+    n:   number of times to compute the RDC and
+         return the median (for stability)
+    According to the paper, the coefficient should be relatively insensitive to
+    the settings of the f, k, and s parameters.
+    
+    Source: https://github.com/garydoranjr/rdc
+    """
+    x = x.reshape((len(x)))
+    y = y.reshape((len(y)))
+    
+    if n > 1:
+        values = []
+        for i in range(n):
+            try:
+                values.append(rdc(x, y, f, k, s, 1))
+            except np.linalg.linalg.LinAlgError: pass
+        return np.median(values)
 
+    if len(x.shape) == 1: x = x.reshape((-1, 1))
+    if len(y.shape) == 1: y = y.reshape((-1, 1))
+
+    # Copula Transformation
+    cx = np.column_stack([rankdata(xc, method='ordinal') for xc in x.T])/float(x.size)
+    cy = np.column_stack([rankdata(yc, method='ordinal') for yc in y.T])/float(y.size)
+
+    # Add a vector of ones so that w.x + b is just a dot product
+    O = np.ones(cx.shape[0])
+    X = np.column_stack([cx, O])
+    Y = np.column_stack([cy, O])
+
+    # Random linear projections
+    Rx = (s/X.shape[1])*np.random.randn(X.shape[1], k)
+    Ry = (s/Y.shape[1])*np.random.randn(Y.shape[1], k)
+    X = np.dot(X, Rx)
+    Y = np.dot(Y, Ry)
+
+    # Apply non-linear function to random projections
+    fX = f(X)
+    fY = f(Y)
+
+    # Compute full covariance matrix
+    C = np.cov(np.hstack([fX, fY]).T)
+
+    # Due to numerical issues, if k is too large,
+    # then rank(fX) < k or rank(fY) < k, so we need
+    # to find the largest k such that the eigenvalues
+    # (canonical correlations) are real-valued
+    k0 = k
+    lb = 1
+    ub = k
+    while True:
+
+        # Compute canonical correlations
+        Cxx = C[:k, :k]
+        Cyy = C[k0:k0+k, k0:k0+k]
+        Cxy = C[:k, k0:k0+k]
+        Cyx = C[k0:k0+k, :k]
+
+        eigs = np.linalg.eigvals(np.dot(np.dot(np.linalg.pinv(Cxx), Cxy),
+                                        np.dot(np.linalg.pinv(Cyy), Cyx)))
+
+        # Binary search if k is too large
+        if not (np.all(np.isreal(eigs)) and
+                0 <= np.min(eigs) and
+                np.max(eigs) <= 1):
+            ub -= 1
+            k = (ub + lb) // 2
+            continue
+        if lb == ub: break
+        lb = k
+        if ub == lb + 1:
+            k = ub
+        else:
+            k = (ub + lb) // 2
+
+    return np.sqrt(np.max(eigs))
